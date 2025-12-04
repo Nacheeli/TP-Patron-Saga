05a7dc7b132d6afe6ec8d164f7f147d07656aaf5
Adriel Alonso <119984417+AdrielA06@users.noreply.github.com>
2025-12-04 15:57:07 -0300

    Configuración docker

--- db/init.sql
+++ db/init.sql
@@ -3,7 +3,7 @@ CREATE DATABASE dev_db;
 CREATE DATABASE prod_db;
 CREATE DATABASE test_db;
 
--- Otorgar privilegios al rol 'admin'
-GRANT ALL PRIVILEGES ON DATABASE dev_db TO admin;
-GRANT ALL PRIVILEGES ON DATABASE prod_db TO admin;
-GRANT ALL PRIVILEGES ON DATABASE test_db TO admin;
+-- Otorgar privilegios al rol REAL que definimos en el .env
+GRANT ALL PRIVILEGES ON DATABASE dev_db TO admin_ecomerce;
+GRANT ALL PRIVILEGES ON DATABASE prod_db TO admin_ecomerce;
+GRANT ALL PRIVILEGES ON DATABASE test_db TO admin_ecomerce;
